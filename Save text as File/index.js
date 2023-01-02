4e9b021ec1ff4d594674d2392e857ba9e85031ec
dula2000 <96098056+dula2000@users.noreply.github.com>
2023-01-02 14:30:48 +0530

    Finalize the code

--- Save text as File/index.js
+++ Save text as File/index.js
@@ -3,6 +3,19 @@ fileNameInput = document.querySelector(".file-name input");
 selectMenu = document.querySelector(".save-as select"),
 saveBtn = document.querySelector(".save-btn");
 
+selectMenu.addEventListener("change",()=>{
+    let selectedOption = selectMenu.options[selectMenu.selectedIndex].text;
+    saveBtn.innerText = `Save As ${selectedOption.split(" ")[0]} File`;
+    console.log();
+});
+
+
 saveBtn.addEventListener("click", ()=>{
-    console.log(selectMenu.value);
-})
+    const blob = new Blob([textarea.value],{type: selectMenu.value});
+    const fileUrl1= URL.createObjectURL(blob);
+    console.log(fileUrl1);
+    const link = document.createElement("a"); //creating <a> tag
+    link.download = fileNameInput.value;//passing file name as download
+    link.href = fileUrl1; // passing fileURL as href value of link
+    link.click(); // clicking link so the file download
+});
